a97a757fa9be92a28cf2044e6a3fa52dbc0d1195
monicajaimesc <monikmaja@gmail.com>
2020-02-23 19:22:11 +0000

    first table working fine

---
+++ create_table.sql
@@ -0,0 +1,2 @@
+-- creates a table called first_table
+CREATE TABLE IF NOT EXISTS proveedor (id INT AUTO_INCREMENT, name VARCHAR(256) NOT NULL , product_id VARCHAR(256) NOT NULL, quantity INT NOT NULL, quantity_buy INT NOT NULL, PRIMARY KEY (id));
